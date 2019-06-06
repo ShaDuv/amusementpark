e036b9f47e149d2d83585803f98dc65121387388
Marina Poltorak and Shawna Duvall <marina@>
2019-06-06 16:25:10 -0700

    fixes the divs not reappearing when staus changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,31 @@ $(document).ready(function () {
     console.log('Click Event is working');
     var puke = parseInt($('input:radio[name=puke]:checked').val());
     var height = parseInt($('#height').val());
+    $('div').show();
+    $('.elserow').hide();
+
     if (height >= 33 && height <= 48) {
       $('.adultrow').hide();
-      $('.middlerow').hide(); {
-        if (puke === 3) {
-          $('.tea').hide();
-          $('.elephant').hide();
-        } else if (puke === 2) {
-          $('.tea').hide();
-        } else {
-          console.log('Rides all the things');
-        };
-      }
+      $('.middlerow').hide();
+      if (puke === 3) {
+        $('.tea').hide();
+        $('.elephant').hide();
+      } else if (puke === 2) {
+        $('.tea').hide();
+      } else {
+        console.log('Rides all the things');
+      };
     } else if (height >= 49 && height <= 60) {
       $('.adultrow').hide();
-      $('.kidrow').hide(); {
-        if (puke === 3) {
-          $('.inversion').hide();
-          $('.mako').hide();
-        } else if (puke === 2) {
-          $('.inversion').hide();
-        } else {
-          console.log('Rides all the things');
-        };
-      }
+      $('.kidrow').hide();
+      if (puke === 3) {
+        $('.inversion').hide();
+        $('.mako').hide();
+      } else if (puke === 2) {
+        $('.inversion').hide();
+      } else {
+        console.log('Rides all the things');
+      };
     } else if (height >= 61 && height <= 84) {
       $('.kidrow').hide(); {
         if (puke === 3) {
@@ -47,7 +48,7 @@ $(document).ready(function () {
       $('.kidrow').hide();
       $('.middlerow').hide();
       $('.adultrow').hide();
-      $('.elserow').removeclass();
+      $('.elserow').show();
       console.log('else condition is working');
     }
   });
