fee6ddae85052cc72dc15ef187d0216cc30e83b9
Marina Poltorak and Shawna Duvall <shawna@>
2019-06-06 13:14:09 -0700

    finishes Jquery for height selection

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,24 @@ $(document).ready(function() {
     console.log("The height " + height + " inches.");
     if (height >= 33 && height <= 48) {
       $(".adultrow").hide();
+      $(".middlerow").hide();
     }
 
     else if (height>= 49 && height<=60 ) {
-      return
+      $(".adultrow").hide();
+      $(".kidrow").hide();
     }
 
     else if (height>= 61 && height<=84 ) {
+      $(".kidrow").hide();
       return
     }
 
     else {
-      return
+      $(".kidrow").hide();
+      $(".middlerow").hide();
+      $(".adultrow").hide();
+      $(".elserow").show();
     }
 
   });
